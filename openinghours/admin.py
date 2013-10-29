0d8179598560970d5a3dc8eed58e66982f555e0e
Philippe O. Wagner <wagner@arteria.ch>
2013-10-29 16:23:56 +0100

    Modeling

--- openinghours/admin.py
+++ openinghours/admin.py
@@ -1,11 +1,14 @@
-"""Admin classes for the openinghours app."""
-# from django.contrib import admin
-
-# from . import models
+from django.contrib import admin
+from .models import *
 
 
 # class YourModelAdmin(admin.ModelAdmin):
 #    list_display = ['some', 'fields', ]
 #    search_fields = ['some', 'fieds', ]
 
+
 # admin.site.register(models.YourModel, YourModelAdmin)
+
+admin.site.register(OpeningHours)
+admin.site.register(ClosingRules)
+admin.site.register(Company)
--- openinghours/models.py
+++ openinghours/models.py
@@ -1,3 +1,46 @@
-"""Just an empty models file to let the testrunner recognize this as app."""
-# from django.db import models
-# from django.utils.translation import ugettext_lazy as _
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+WEEKDAYS = [
+    (1, _("Monday")),
+    (2, _("Tuesday")),
+    (3, _("Wednesday")),
+    (4, _("Thursday")),
+    (5, _("Friday")),
+    (6, _("Saturday")),
+    (7, _("Sunday")),
+]
+
+
+class Company(models.Model):
+    '''
+    '''
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    logo = models.FileField(upload_to='logo', null=True, blank=True)
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.name, self.slug)
+
+
+class OpeningHours(models.Model):
+    '''
+    '''
+    company = models.ForeignKey(Company)
+    weekday = models.IntegerField(choices=WEEKDAYS)
+    fromHour = models.TimeField()
+    toHour = models.TimeField()
+
+
+class ClosingRules(models.Model):
+    '''
+    Used to overrule the OpeningHours. This will "close" the store due to 
+    public holiday, annual closing or private party, etc.
+    '''
+    company = models.ForeignKey(Company)
+    start = models.DateTimeField() 
+    end = models.DateTimeField()
+    reason = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return "%s is closed from %s to %s due to %s" % (self.company.anme, str(self.start), str(self.end), self.reason)
