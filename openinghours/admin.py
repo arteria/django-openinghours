f0a7fd6690c6b6e2b5cef0f1ff64b44e398ac086
F <fmalina@gmail.com>
2016-02-06 19:06:40 +0000

    Upgraded admin to make our company model optional.

--- openinghours/admin.py
+++ openinghours/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from openinghours.models import OpeningHours, ClosingRules, Company
+from openinghours.models import (OpeningHours, ClosingRules, Company,
+    OPENINGHOURS_PREMISES_MODEL)
 
 
 admin.site.register(OpeningHours)
 admin.site.register(ClosingRules)
-admin.site.register(Company)
+if OPENINGHOURS_PREMISES_MODEL == 'openinghours.models.Company':
+    admin.site.register(Company)
