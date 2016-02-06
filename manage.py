8b1402be0c8bbb80ad7e575035cf22f2c3d320d4
F <fmalina@gmail.com>
2016-02-06 21:31:42 +0000

    Removed south as test dependency.

--- manage.py
+++ manage.py
@@ -3,9 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'openinghours.tests.south_settings')
-
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openinghours.tests.test_settings')
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
