71f4e52c88c94813dfef7cb7882c79f7b54c6c1a
F <fmalina@gmail.com>
2016-02-06 20:36:05 +0000

    i10n

--- openinghours/models.py
+++ openinghours/models.py
@@ -27,9 +27,9 @@ class Company(models.Model):
         verbose_name = 'Company'
         verbose_name_plural = 'Companies'
     
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
-    logo = models.FileField(upload_to='logo', null=True, blank=True)
+    name = models.CharField(_('Name'), max_length=100)
+    slug = models.SlugField(_('Slug'), unique=True)
+    logo = models.FileField(_('Logo'), upload_to='logo', null=True, blank=True)
     
     def __str__(self):
         return "%s (%s)" % (self.name, self.slug)
@@ -40,13 +40,13 @@ class OpeningHours(models.Model):
     """
     """
     class Meta:
-        verbose_name = 'Opening Hour'
-        verbose_name_plural = 'Opening Hours'
+        verbose_name = _('Opening Hours') # plurale tantum
+        verbose_name_plural = _('Opening Hours')
         
-    company = models.ForeignKey(PREMISES_MODEL)
-    weekday = models.IntegerField(choices=WEEKDAYS)
-    from_hour = models.TimeField()
-    to_hour = models.TimeField()
+    company = models.ForeignKey(PREMISES_MODEL, verbose_name=_('Company'))
+    weekday = models.IntegerField(_('Weekday'), choices=WEEKDAYS)
+    from_hour = models.TimeField(_('Opening'))
+    to_hour = models.TimeField(_('Closing'))
 
     def __str__(self):
         return "%s %s (%s - %s)" % (self.company, self.weekday, self.from_hour, self.to_hour)
@@ -59,13 +59,13 @@ class ClosingRules(models.Model):
     public holiday, annual closing or private party, etc.
     """
     class Meta:
-        verbose_name = 'Closing Rule'
-        verbose_name_plural = 'Closing Rules'
+        verbose_name = _('Closing Rule')
+        verbose_name_plural = _('Closing Rules')
         
-    company = models.ForeignKey(PREMISES_MODEL)
-    start = models.DateTimeField() 
-    end = models.DateTimeField()
-    reason = models.TextField(null=True, blank=True)
+    company = models.ForeignKey(PREMISES_MODEL, verbose_name=_('Company'))
+    start = models.DateTimeField(_('Start')) 
+    end = models.DateTimeField(_('End'))
+    reason = models.TextField(_('Reason'), null=True, blank=True)
 
     def __str__(self):
         return "%s is closed from %s to %s due to %s" % (self.company.name, str(self.start), str(self.end), self.reason)
