00b9b4eb8c294067b9f0bf012e11ac1f9372fea9
F <fmalina@gmail.com>
2016-02-07 00:46:25 +0000

    Make admin to play nicely with 10,000 companies. - Companies will need admin inlines

--- openinghours/admin.py
+++ openinghours/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from openinghours.models import OpeningHours, ClosingRules, Company, PREMISES_MODEL
 
+class OpeningHoursAdmin(admin.ModelAdmin):
+    raw_id_fields = ('company',)
 
-admin.site.register(OpeningHours)
+admin.site.register(OpeningHours, OpeningHoursAdmin)
 admin.site.register(ClosingRules)
 if PREMISES_MODEL == 'openinghours.Company':
     admin.site.register(Company)
