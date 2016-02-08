4230036489a417f8f5256b002229da996f4785f2
F <fmalina@gmail.com>
2016-02-08 19:00:33 +0000

    Improved admin

--- openinghours/admin.py
+++ openinghours/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from openinghours.models import OpeningHours, ClosingRules, Company, PREMISES_MODEL
+from openinghours.models import OpeningHours, ClosingRules, Company
+from app_settings import PREMISES_MODEL
 
-class OpeningHoursAdmin(admin.ModelAdmin):
-    raw_id_fields = ('company',)
+class OpeningHoursInline(admin.TabularInline):
+    model = OpeningHours
+    extra = 0
 
-admin.site.register(OpeningHours, OpeningHoursAdmin)
-admin.site.register(ClosingRules)
+class ClosingRulesInline(admin.StackedInline):
+    model = ClosingRules
+    extra = 0
+
+class CompanyAdmin(admin.ModelAdmin):
+    inlines = [OpeningHoursInline, ClosingRulesInline]
+    search_fields = ['name','slug']
+
+# OPENINGHOURS_PREMISES_MODEL users need to register their own admin from their app
 if PREMISES_MODEL == 'openinghours.Company':
-    admin.site.register(Company)
+    admin.site.register(Company, CompanyAdmin)
