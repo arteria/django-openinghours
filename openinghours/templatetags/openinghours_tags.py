f4e0c83f8ca6f6b745fe4546920c2576c100b3c8
F <fmalina@gmail.com>
2016-02-15 18:41:10 +0000

    PEP8

--- openinghours/templatetags/openinghours_tags.py
+++ openinghours/templatetags/openinghours_tags.py
@@ -1,15 +1,15 @@
 import datetime
 
-from django import template
+from django.template import Library
+from django.template.loader import get_template, render_to_string
 from django.utils.safestring import mark_safe
-from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
 from openinghours.models import *
 from openinghours import utils
 
 
-register = template.Library()
+register = Library()
 
 
 @register.filter(expects_localtime=True)
@@ -20,15 +20,18 @@ def iso_day_to_weekday(d):
         if w[0] == int(d):
             return w[1]
 
+
 @register.filter(expects_localtime=True)
 def to_weekday(date_obj_tpl):
     oh, date_obj = date_obj_tpl
     now = utils.get_now()
-    if date_obj.isoweekday() == now.isoweekday() and (date_obj - now).days == 0:
+    day = date_obj.isoweekday()
+    if day == now.isoweekday() and (date_obj - now).days == 0:
         return _("today")
     for w in WEEKDAYS:
-        if w[0] == int(date_obj.isoweekday()):
+        if w[0] == int(day):
             return w[1]
+
 
 @register.assignment_tag
 def is_open(location=None, attr=None):
@@ -36,47 +39,53 @@ def is_open(location=None, attr=None):
     if obj is False:
         return False
     if attr is not None:
-        return getattr(obj, attr) 
+        return getattr(obj, attr)
     return obj
-    
+
+
 @register.assignment_tag
 def next_time_open(location):
     obj, ts = utils.next_time_open(location)
     return obj
 
-@register.filter(expects_localtime=True) 
+
+@register.filter(expects_localtime=True)
 def has_closing_rule_for_now(location, attr=None):
     obj = utils.has_closing_rule_for_now(location)
     if obj is False:
         return False
     if attr is not None:
-        return getattr(obj, attr) 
+        return getattr(obj, attr)
     return obj
 
-@register.filter(expects_localtime=True) 
+
+@register.filter(expects_localtime=True)
 def get_closing_rule_for_now(location, attr=None):
-    """ Only accesses the *first* closing rule, because closed means closed. """
+    """Only accesses the *first* closing rule, because closed means closed."""
     obj = utils.get_closing_rule_for_now(location)
     if obj is False:
         return False
     if attr is not None:
-        return getattr(obj[0], attr) 
+        return getattr(obj[0], attr)
     return obj
-    
+
+
 @register.simple_tag
 def opening_hours(location=None, concise=False):
-    """ Creates a rendered listing of hours. """
+    """Creates a rendered listing of hours."""
     template_name = 'openinghours/opening_hours_list.html'
-    days = [] # [{'hours': '9:00am to 5:00pm', 'name': u'Monday'}, {'hours': '9:00am to...
+    days = []  # [{'hours': '9:00am to 5:00pm', 'name': u'Monday'}, {'hours...
 
     # Without `location`, choose the first company.
-    if location: 
+    if location:
         ohrs = OpeningHours.objects.filter(company=location)
     else:
         try:
-            ohrs = utils.get_premises_model().objects.first().openinghours_set.all()
+            Location = utils.get_premises_model()
+            ohrs = Location.objects.first().openinghours_set.all()
         except AttributeError:
-            raise Exception("You must define some opening hours to use the opening hours tags.")
+            raise Exception("You must define some opening hours"
+                            " to use the opening hours tags.")
 
     ohrs.order_by('weekday', 'from_hour')
 
@@ -86,9 +95,9 @@ def opening_hours(location=None, concise=False):
             'from_hour': o.from_hour,
             'to_hour': o.to_hour,
             'hours': '%s%s to %s%s' % (
-                o.from_hour.strftime('%I:%M').lstrip('0'), 
+                o.from_hour.strftime('%I:%M').lstrip('0'),
                 o.from_hour.strftime('%p').lower(),
-                o.to_hour.strftime('%I:%M').lstrip('0'), 
+                o.to_hour.strftime('%I:%M').lstrip('0'),
                 o.to_hour.strftime('%p').lower()
             )
         })
@@ -100,31 +109,34 @@ def opening_hours(location=None, concise=False):
             })
 
     if concise:
-        # [{'hours': '9:00am to 5:00pm', 'day_names': u'Monday to Friday'}, {'hours':...
+        # [{'hours': '9:00am to 5:00pm', 'day_names': u'Monday to Friday'},
+        #  {'hours':...
         template_name = 'openinghours/opening_hours_list_concise.html'
         concise_days = []
         current_set = {}
         for day in days:
             if 'hours' not in current_set.keys():
-                current_set = {'day_names': [day['name'],], 'hours': day['hours']}
+                current_set = {'day_names': [day['name']],
+                               'hours': day['hours']}
             elif day['hours'] != current_set['hours']:
                 concise_days.append(current_set)
-                current_set = {'day_names': [day['name'],], 'hours': day['hours']}
+                current_set = {'day_names': [day['name']],
+                               'hours': day['hours']}
             else:
                 current_set['day_names'].append(day['name'])
         concise_days.append(current_set)
 
         for day_set in concise_days:
             if len(day_set['day_names']) > 2:
-                day_set['day_names'] = '%s to %s' % (day_set['day_names'][0], day_set['day_names'][-1])
+                day_set['day_names'] = '%s to %s' % (day_set['day_names'][0],
+                                                     day_set['day_names'][-1])
             elif len(day_set['day_names']) > 1:
-                day_set['day_names'] = '%s and %s' % (day_set['day_names'][0], day_set['day_names'][-1])
+                day_set['day_names'] = '%s and %s' % (day_set['day_names'][0],
+                                                      day_set['day_names'][-1])
             else:
                 day_set['day_names'] = '%s' % day_set['day_names'][0]
-        
+
         days = concise_days
 
-    t = template.loader.get_template(template_name)
-    return t.render({
-        'days': days
-    })
+    template = get_template(template_name)
+    return template.render({'days': days})
--- openinghours/tests/tests.py
+++ openinghours/tests/tests.py
@@ -1,50 +1,56 @@
 from django.test import TestCase, Client
 from openinghours.models import Company, OpeningHours, ClosingRules
-import datetime
+from openinghours.forms import str_to_time
+from datetime import datetime, timedelta
+
 
 class OpeningHoursTestCase(TestCase):
     """Test some possible business scenarios."""
     def setUp(self):
         """Create test opening hours and holidays:
-        
+
         - late opening for Monday and Tuesday
         - some planned breaks
         - closed on a Sunday
         """
         test_data = {
-            1: [[( 8,30), (12,00)], [(12,30), (22,00)]],
-            2: [[( 8,30), (12,00)], [(12,30), (18,00)], [(18,30), (22,00)]],
-            3: [[( 9,00), (17,00)]],
-            4: [[( 9,00), (17,00)]],
-            5: [[( 9,00), (17,00)]],
-            6: [[(10,00), (13,00)]],
+            1: ['08:30 12:00', '12:30 22:00'],
+            2: ['08:30 12:00', '12:30 18:00', '18:30 22:00'],
+            3: ['09:00 17:00'],
+            4: ['09:00 17:00'],
+            5: ['09:00 17:00'],
+            6: ['10:00 13:00'],
             7: None,
         }
-        company, created = Company.objects.get_or_create(name="Company Ltd.", slug="company-ltd")
+        company, created = Company.objects.get_or_create(name="Company Ltd.",
+                                                         slug="company-ltd")
         for day, hours in test_data.items():
             if not hours:
                 continue
-            for from_hour, to_hour in hours:
+            for slot in hours:
+                from_hour, to_hour = slot.split()
                 OpeningHours.objects.get_or_create(
-                    company   = company,
-                    weekday   = day,
-                    from_hour = datetime.time(*from_hour),
-                    to_hour   = datetime.time(*to_hour)
+                    company=company,
+                    weekday=day,
+                    from_hour=str_to_time(from_hour),
+                    to_hour=str_to_time(to_hour)
                 )
+
+        def days_ahead(d): return datetime.today() + timedelta(days=d)
+
         # setup some holidays
-        today = datetime.datetime.today()
         holiday = ClosingRules.objects.create(
-            company = company,
-            start  = today+datetime.timedelta(days=2),
-            end    = today+datetime.timedelta(days=4),
-            reason = "Public holiday"
+            company=company,
+            start=days_ahead(2),
+            end=days_ahead(4),
+            reason="Public holiday"
         )
-    
+
     def tearDown(self):
         """Reset state by hand while developing tests in the shell."""
         for o in OpeningHours.objects.all():
             o.delete()
-    
+
     def test_hours_are_published(self):
         response = Client().get('/')
         self.assertContains(response, '8:30am to 12:00pm')
