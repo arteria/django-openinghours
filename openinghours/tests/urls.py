95759a933d5ce2885dcbeb06d6e8065e28517e19
F <fmalina@gmail.com>
2016-02-15 10:43:14 +0000

    Updated test URLs.

--- openinghours/tests/urls.py
+++ openinghours/tests/urls.py
@@ -1,11 +1,10 @@
 """URLs to run the tests."""
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
-
+from openinghours.urls import urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns += [
     url(r'^admin/', include(admin.site.urls)),
-)
+]
