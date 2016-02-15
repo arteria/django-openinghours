fbe47f75f11452d1f5747c7620233e5bebd0d9a9
F <fmalina@gmail.com>
2016-02-15 23:25:30 +0000

    Added POST data processing prototype.

--- openinghours/views_edit.py
+++ openinghours/views_edit.py
@@ -12,6 +12,8 @@ def edit(request, pk):
     Location = utils.get_premises_model()
     location = get_object_or_404(Location, pk=pk)
     
+    def prefix(day_n, slot_n): return "day%s_%s" % (day_n, slot_n)
+    
     # build a lookup dictionary to populate the form slots
     # day numbers are keys, list of opening hours for that day are values
     hours = {}
@@ -36,16 +38,29 @@ def edit(request, pk):
             if ini2:
                 two_sets = True
 
-        pre = "day%s_" % day_n
         week.append({
             'weekday': day,
-            'slot1': Slot(prefix=pre+'1', initial=ini1),
-            'slot2': Slot(prefix=pre+'2', initial=ini2),
+            'slot1': Slot(prefix=prefix(day_n, 1), initial=ini1),
+            'slot2': Slot(prefix=prefix(day_n, 2), initial=ini2),
             'closed': closed
         })
 
     if request.method == 'POST':
-        pass
+        # low level processing of the raw POST data
+        post = dict(request.POST)
+        data = []
+        for k,v in post.items():
+            if not k.startswith('day'):
+                continue
+            day_id, time_id = k.split('_')
+            day_n = int(day_id[-1])   # 1 for Monday, 2 for Tuesday, 3 for ...
+            slot_n = int(time_id[0])  # 1 for morning slot, 2 for afternoon
+            open_shut = time_id[2:]   # string 'opens' or 'shuts'
+            time = str_to_time(v[0])  # time object: time(15, 30)
+            data.append((day_n, slot_n, time, open_shut))
+        data = sorted(data)
+        from pprint import pprint
+        pprint(data)
 
     return render(request, "openinghours/form.html", {
         'week': week,
