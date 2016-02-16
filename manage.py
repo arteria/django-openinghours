5297a833a2811180f3ed0b65a129174f1660bd2c
F <fmalina@gmail.com>
2016-02-16 13:43:28 +0000

    Formatting, JS comments

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault(
-        'DJANGO_SETTINGS_MODULE', 'openinghours.tests.test_settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'openinghours.tests.test_settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
