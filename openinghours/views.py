33db59eea18956ba0700aedcc275f403ab15015b
F. Malina <fmalina@gmail.com>
2016-04-01 01:13:16 +0100

    Update views.py
    Address #14
    
    Custom PREMISES_MODEL wasn't implemented here as this view only served as an example, but it's more complete this way indeed.

--- openinghours/views.py
+++ openinghours/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import TemplateView
-from openinghours.models import Company
+from openinghours.utils import get_premises_model
 
 
 class CurrentlyOpenView(TemplateView):
+    model = get_premises_model()
     template_name = "openinghours/index.html"
 
     def get_context_data(self, **kwargs):
         context = super(CurrentlyOpenView, self).get_context_data(**kwargs)
-        context['location'] = Company.objects.first()
+        context['location'] = self.model.objects.first()
         return context
