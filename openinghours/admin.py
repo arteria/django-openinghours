92d0aa96669ff144a838d0790f2369581d6ad7b1
Philippe O. Wagner <wagner@arteria.ch>
2016-05-08 23:29:02 +0200

    admin site support for transmeta

--- openinghours/admin.py
+++ openinghours/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from transmeta import canonical_fieldname
 
 
 # class YourModelAdmin(admin.ModelAdmin):
@@ -9,6 +10,12 @@ from .models import *
 
 # admin.site.register(models.YourModel, YourModelAdmin)
 
+class ClosingRulesAdmin(admin.ModelAdmin):
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        field = super(ClosingRulesAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        db_fieldname = canonical_fieldname(db_field)
+        return field
+
 admin.site.register(OpeningHours)
-admin.site.register(ClosingRules)
+admin.site.register(ClosingRules, ClosingRulesAdmin)
 admin.site.register(Company)
