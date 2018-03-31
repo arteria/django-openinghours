39abf2efcd8e14e951020b35951f7a6f853976ab
F. Malina <fmalina@gmail.com>
2018-03-31 14:16:04 +0100

    Added on_delete to 2nd migration

--- openinghours/migrations/0002_auto_20170915_0912.py
+++ openinghours/migrations/0002_auto_20170915_0912.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='closingrules',
             name='company',
-            field=models.ForeignKey(to='openinghours.Company', verbose_name='Company'),
+            field=models.ForeignKey(to='openinghours.Company', verbose_name='Company', on_delete=models.CASCADE),
         ),
         migrations.AlterField(
             model_name='closingrules',
@@ -57,7 +57,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='openinghours',
             name='company',
-            field=models.ForeignKey(to='openinghours.Company', verbose_name='Company'),
+            field=models.ForeignKey(to='openinghours.Company', verbose_name='Company', on_delete=models.CASCADE),
         ),
         migrations.AlterField(
             model_name='openinghours',
