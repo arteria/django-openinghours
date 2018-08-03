88ad7b15a0576c77eb3ebbf75d9fa40f15488a99
Camron Flanders <camron.flanders@gmail.com>
2018-08-03 13:50:11 -0500

    update templatetag to use django utils timezone

--- openinghours/templatetags/openinghours_tags.py
+++ openinghours/templatetags/openinghours_tags.py
@@ -1,7 +1,9 @@
+import datetime
+
 from django.template import Library
 from django.template.loader import get_template
 from django.utils.translation import ugettext_lazy as _
-import datetime
+from django.utils import timezone
 
 from openinghours.models import WEEKDAYS, OpeningHours
 from openinghours import utils
@@ -54,9 +56,9 @@ def is_open_now(location=None, attr=None):
     """
     Returns False if the location is closed, or the OpeningHours object
     to show the location is currently open.
-    Same as `is_open` but passes `now` to `utils.is_open` to bypass `get_now()`. 
+    Same as `is_open` but passes `now` to `utils.is_open` to bypass `get_now()`.
     """
-    obj = utils.is_open(location, now=datetime.datetime.now())
+    obj = utils.is_open(location, now=timezone.now())
     if obj is False:
         return False
     if attr is not None:
