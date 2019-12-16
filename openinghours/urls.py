13c373cdb15f994fddff2e54ce622c7c4936e367
F. Malina <fmalina@pm.me>
2019-12-16 12:18:03 +0100

    Updated URLs to use path
    Dropping Django 1.11 support here.

--- openinghours/urls.py
+++ openinghours/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from openinghours.views import CurrentlyOpenView
 from openinghours.views_edit import OpeningHoursEditView
 
 urlpatterns = [
-     url(r'^$', CurrentlyOpenView.as_view(),
-         name='openinghours_currently_open'),
-     url(r'^edit/(?P<pk>[0-9]+)$', OpeningHoursEditView.as_view(),
-         name='openinghours_edit'),
+     path('', CurrentlyOpenView.as_view(),
+          name='openinghours_currently_open'),
+     path('edit/<int:pk>', OpeningHoursEditView.as_view(),
+          name='openinghours_edit'),
 ]
